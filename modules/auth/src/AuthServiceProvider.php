7535ba61017ad4283d27eb8d0c0e594a01412338
Dmitry Churkin <dmitrychurkin1988@gmail.com>
2024-12-28 00:07:31 +0000

    clean up app service provider;

--- modules/auth/src/AuthServiceProvider.php
+++ modules/auth/src/AuthServiceProvider.php
@@ -7,6 +7,7 @@ namespace AppointmentService\Auth;
 use AppointmentService\Auth\View\Components\AppLayout;
 use AppointmentService\Auth\View\Components\GuestLayout;
 use AppointmentService\Common\Providers\ServiceProvider;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\Facades\Blade;
 
 final class AuthServiceProvider extends ServiceProvider
@@ -28,7 +29,26 @@ final class AuthServiceProvider extends ServiceProvider
             __DIR__.'/../database/migrations' => database_path('migrations'),
         ], 'auth-migrations');
 
+        $this->registerComponents()
+            ->configureResetPassword();
+    }
+
+    private function registerComponents(): self
+    {
         Blade::component('app-layout', AppLayout::class);
         Blade::component('guest-layout', GuestLayout::class);
+
+        return $this;
+    }
+
+    private function configureResetPassword(): self
+    {
+        ResetPassword::createUrlUsing(fn (object $notifiable, string $token) => (
+            config('app.frontend_url').url()->query("/password-reset/{$token}", [
+                'email' => $notifiable->getEmailForPasswordReset(),
+            ])
+        ));
+
+        return $this;
     }
 }

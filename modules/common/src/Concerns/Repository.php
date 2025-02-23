a64a1d90c77f1ce85fc1112ee2e822f8824d3ce7
Dmitry Churkin <dmitrychurkin1988@gmail.com>
2025-02-23 23:11:19 +0000

    - enhance model abstraction on repositories;

---
+++ modules/common/src/Concerns/Repository.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppointmentService\Common\Concerns;
+
+use Illuminate\Database\Eloquent\Builder;
+
+trait Repository
+{
+    private Builder $query {
+        get => $this->query ?: $this->model::query();
+        set => $this->query = $value;
+    }
+
+    public function withQuery(Builder $query): self
+    {
+        $this->query = $query;
+
+        return $this;
+    }
+}

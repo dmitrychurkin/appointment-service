33ccc41b231d66fcae6a70aae89b3cdf2166fa83
Dmitry Churkin <dmitrychurkin1988@gmail.com>
2025-03-01 20:37:50 +0000

    feat: Implementation general availability;

--- modules/appointment/database/migrations/2025_02_25_234350_create_configuration_recurrences_table.php
+++ modules/appointment/database/migrations/2025_02_25_234350_create_configuration_recurrences_table.php
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('configuration_recurrence');
+        Schema::dropIfExists('configuration_recurrences');
     }
 };
